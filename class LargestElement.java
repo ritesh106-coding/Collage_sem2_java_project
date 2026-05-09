919cd15a2e81c5caa0333648f6cfe3122ae181fc
Ritesh <rp0800385@gmail.com>
2026-05-09 21:33:18 +0530

    Add LargestElement class to find largest number

---
+++ class LargestElement.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class LargestElement
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n, largest;
+
+        System.out.println("Enter the number of elements:");
+        n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        largest = arr[0];
+
+        for(int i = 1; i < n; i++)
+        {
+            if(arr[i] > largest)
+            {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest element = " + largest);
+    }
+}
